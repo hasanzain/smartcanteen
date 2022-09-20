5dad553740e84dcf9224f0a09128bd72a5da882f
hasan zain <hasanzain045@gmail.com>
2022-09-20 15:49:28 +0700

    update logic makan

--- application/controllers/AbsenMakan.php
+++ application/controllers/AbsenMakan.php
@@ -35,25 +35,31 @@ class AbsenMakan extends REST_Controller
     }
 
 
-    function cekMakan($id_karyawan, $query, $limit)
+    function cekMakan($id_karyawan, $query, $limit, $waktu)
     {
         // $id = 12345678;
+        if ($waktu == "sore") {
+            $this->db->where('keterangan', $waktu);
+        }
         $this->db->where('id_karyawan', $id_karyawan);
         $this->db->where('tanggal', date("Y-m-d"));
-        $riwayat_makan = $this->db->get('riwayat_makan')->result_array();
+        $riwayat_makan = $this->db->get('riwayat_makan')->num_rows();
         // return count($riwayat_makan);
 
         if ($query == "<=") {
-            $param = count($riwayat_makan) <= $limit;
+            $param = $riwayat_makan <= $limit ? true : false;
         } else {
-            $param = count($riwayat_makan) < $limit;
+            $param = $riwayat_makan < $limit ? true : false;
         }
+        // var_dump($param);
+        // die;
+        return $param;
 
-        if ($param) {
-            return true;
-        } else {
-            return false;
-        }
+        // if ($param) {
+        //     return true;
+        // } else {
+        //     return false;
+        // }
     }
 
     function ambilMakan($id, $keterangan)
@@ -65,6 +71,8 @@ class AbsenMakan extends REST_Controller
         $karyawan = $this->db->get('karyawan');
 
         if ($karyawan->num_rows() > 0) {
+            // var_dump($karyawan->num_rows());
+            // die;
 
             foreach ($karyawan->result_array() as $row) {
                 $id_karyawan = $row['id_karyawan'];
@@ -80,6 +88,8 @@ class AbsenMakan extends REST_Controller
                     'jam' => date("H:i:s"),
                     'keterangan' => $keterangan,
                 );
+                // var_dump($this->db->insert('riwayat_makan', $data));
+                // die;
 
                 if ($this->db->insert('riwayat_makan', $data)) {
                     $this->response(array('Status' => 'SILAHKAN AMBIL'), 200);
@@ -102,52 +112,56 @@ class AbsenMakan extends REST_Controller
         // var_dump($id);
         // die;
 
-        $mydate = getdate(date("U"));
-        $jam = (int) $mydate["hours"];
-        $menit = (int) $mydate["minutes"];
+        // $mydate = getdate(date("U"));
+        // $jam = (int) $mydate["hours"];
+        // $menit = (int) $mydate["minutes"];
 
-        // $jam = 7;
-        // $menit = 59;
+        $jam = 17;
+        $menit = 30;
 
-        // $this->db->where('id_karyawan', $id);
-        // $karyawan = $this->db->get('karyawan');
+        $this->db->where('id_karyawan', $id);
+        $karyawan = $this->db->get('karyawan');
         // var_dump($karyawan->num_rows());
         // die;
 
-        // if ($karyawan) {
+        if ($karyawan->num_rows() > 0) {
 
 
+            // var_dump($this->cekMakan($id, "<", 3));
+            // die;
 
-        if ($jam >= $this->getTime("jam", "pagi") && $jam < $this->getTime("jam", "siang")) {
-            if ($menit >= $this->getTime("menit", "pagi")) {
-                if ($this->cekMakan($id, "<", 1)) {
-                    $this->ambilMakan($id, "pagi");
-                } else {
-                    $this->response(array('Status' => 'SUDAH MAKAN!'), 200);
+            // if ($jam >= $this->getTime("jam", "pagi") && $jam < $this->getTime("jam", "siang")) {
+            if ($jam >= $this->getTime("jam", "pagi") && $jam < 12) {
+                if ($menit >= $this->getTime("menit", "pagi")) {
+                    if ($this->cekMakan($id, "<=", 1, "pagi")) {
+                        $this->ambilMakan($id, "pagi");
+                    } else {
+                        $this->response(array('Status' => 'SUDAH MAKAN!'), 200);
+                    }
                 }
-            }
-        } elseif ($jam >= $this->getTime("jam", "siang") && $jam < $this->getTime("jam", "sore")) {
-            if ($menit >= $this->getTime("menit", "siang")) {
-                if ($this->cekMakan($id, "<=", 2)) {
-                    $this->ambilMakan($id, "siang");
-                } else {
-                    $this->response(array('Status' => 'SUDAH MAKAN!'), 200);
+            } elseif ($jam >= 12 && $jam <= $this->getTime("jam", "siang")) {
+                // } elseif ($jam >= $this->getTime("jam", "siang") && $jam < $this->getTime("jam", "sore")) {
+                if ($menit <= $this->getTime("menit", "siang")) {
+                    if ($this->cekMakan($id, "<", 2, "siang")) {
+                        $this->ambilMakan($id, "siang");
+                    } else {
+                        $this->response(array('Status' => 'SUDAH MAKAN!'), 200);
+                    }
                 }
-            }
-        } elseif ($jam >= $this->getTime("jam", "sore") && $jam < 21) {
-            if ($menit >= $this->getTime("menit", "sore")) {
-                if ($this->cekMakan($id, "<", 3)) {
-                    $this->ambilMakan($id, "sore");
-                } else {
-                    $this->response(array('Status' => 'SUDAH MAKAN!'), 200);
+            } elseif ($jam >= $this->getTime("jam", "sore") && $jam < 21) {
+                if ($menit >= $this->getTime("menit", "sore")) {
+                    if ($this->cekMakan($id, "<", 1, "sore")) {
+                        $this->ambilMakan($id, "sore");
+                    } else {
+                        $this->response(array('Status' => 'SUDAH MAKAN!'), 200);
+                    }
                 }
+            } else {
+                $this->response(array('Status' => 'DILUAR WAKTU!'), 200);
             }
         } else {
-            $this->response(array('Status' => 'DILUAR WAKTU!'), 200);
+            $this->response(array('Status' => 'TIDAK TERDAFTAR!'), 200);
         }
-        // } else {
-        //     $this->response(array('Status' => 'TIDAK TERDAFTAR!'), 200);
-        // }
     }
 
     function index_post()
